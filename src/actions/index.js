d4eac953cf516224fa263fc62b6cfa3c64d26a51
Jeremy Hon <jehon11@gmail.com>
2019-04-07 22:32:16 +0800

    can post to api and update message list

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,31 @@
+/* eslint-disable import/prefer-default-export */
 // TODO: add and export your own actions
+export const FETCH_MESSAGES = "FETCH_MESSAGES";
+export const MESSAGE_POSTED = "MESSAGE_POSTED";
+
+
+export function fetchMessages(channel) {
+  const promise = fetch(`https://wagon-chat.herokuapp.com/${channel}/messages`)
+    .then(response => response.json());
+  return {
+    type: FETCH_MESSAGES,
+    payload: promise
+  };
+}
+
+export function createMessage(channel, author, content) {
+  const body = { author, content }
+  const promise = fetch(`https://wagon-chat.herokuapp.com/${channel}/messages`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  }).then(response => response.json());
+
+  return {
+    type: MESSAGE_POSTED,
+    payload: promise
+  };
+}
--- src/containers/message_list.jsx
+++ src/containers/message_list.jsx
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchMessages } from '../actions/index'
 import Message from '../components/message';
 
-const MessageList = ({ messages }) => {
-  return (
-    <div>
-      {messages.map(message => <Message message={message} key={message.id} />)}
-    </div>
-  );
-};
+class MessageList extends Component {
+  componentWillMount() {
+    this.fetchMessages();
+  }
 
+  fetchMessages = () => {
+    this.props.fetchMessages(this.props.selectedChannel)
+  }
+
+  render() {
+    const { messages } = this.props
+    return (
+      <div>
+        {messages.map(message => <Message message={message} key={message.id} />)}
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
-    messages: state.messages
+    messages: state.messages,
+    selectedChannel: state.selectedChannel
   };
 };
 
-export default connect(mapStateToProps)(MessageList);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { fetchMessages },
+    dispatch
+  );
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MessageList);
